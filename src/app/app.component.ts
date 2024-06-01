984814095db32fab2526968deb77fb04f64c0df2
red_minter <oleg.a.fedyshyn@gmail.com>
2024-06-01 19:48:05 +0300

    changed template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, NavigationEnd, Router} from "@angular/router";
+import {filter, map} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,29 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AppComponent{
   title = 'minivns';
+
+  showHeaderAndSidebar: boolean = true;
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => this.activatedRoute),
+      map(route => {
+        while (route.firstChild) {
+          route = route.firstChild;
+        }
+        return route;
+      }),
+      filter(route => route.outlet === 'primary'),
+      map(route => route.snapshot)
+    ).subscribe(snapshot => {
+      const url = snapshot.url.map(segment => segment.path).join('/');
+      this.showHeaderAndSidebar = !this.shouldHideHeaderAndSidebar(url);
+    });
+  }
+
+  shouldHideHeaderAndSidebar(url: string): boolean {
+    const routesToHide = ['/login', '/error'];
+    return routesToHide.some(route => url.includes(route)) || url === '';
+  }
 }
